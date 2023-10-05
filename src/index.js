7c72800dbf7e4c7307a77c6aaecfec1607bd969f
kevit-keval-kankrecha <kevit.keval.kankrecha@gmail.com>
2023-10-05 14:36:35 +0530

    Changes the index.js structure after adding logger winston

--- src/index.js
+++ src/index.js
@@ -26,12 +26,12 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
-        socket.emit('message', generateMessage('Admin','Welcome!'));
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined!`));
+        socket.emit('message', generateMessage('Admin', 'Welcome!'));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`));
 
-        io.to(user.room).emit('roomData',{
-            room:user.room,
-            users:getUsersInRoom(user.room)
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
         })
     })
 
@@ -47,16 +47,16 @@ io.on('connection', (socket) => {
         }
 
         //get user by their id
-        const user=getUser(socket.id)
+        const user = getUser(socket.id)
 
-        io.to(user.room).emit('message', generateMessage(user.username,message));
+        io.to(user.room).emit('message', generateMessage(user.username, message));
         callback('Delivered');
     });
 
     //receive location from clients nd share to other clients
     socket.on('sendLocation', (location, callback) => {
-        const user=getUser(socket.id);
-        io.to(user.room).emit('messageLocation', generateLocationMessage(user.username,`https://www.google.com/maps?q=${location.latitude},${location.longitude}`));
+        const user = getUser(socket.id);
+        io.to(user.room).emit('messageLocation', generateLocationMessage(user.username, `https://www.google.com/maps?q=${location.latitude},${location.longitude}`));
         callback('Location Shared');
     });
 
@@ -64,10 +64,10 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const user = removeUser(socket.id);
         if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} is left!`));
-            io.to(user.room).emit('roomData',{
-                room:user.room,
-                users:getUsersInRoom(user.room)
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} is left!`));
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
             })
         }
     });
